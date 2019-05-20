ceb9c44dd116e8f645e8c954b4e5b8f83d779a3d
Elhamdani <spartan9452@gmail.com>
2019-05-20 10:06:23 +0200

    e

---
+++ app/components/Game.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="big-header" :class="addScene">
+    <h1></h1>
+    <br />
+    <ul ref="msgList" class="msg-list">
+      <transition-group name="list">
+        <li class="message" :class="{'user-choice': isUserChoice(msg) }" v-for="msg in storagedMsg" :key="msg">{{msg}}</li>
+      </transition-group>
+    </ul>
+    <br>
+    <br>
+    <div class="btn-container">
+      <router-link class="button" :to="actionA">{{btnA}}
+        <button class="in-button" v-on:click="addChoiceOnTable(btnA)"></button>
+      </router-link>
+      <router-link class="button" :to="actionB">{{btnB}}
+        <button class="in-button" v-on:click="addChoiceOnTable(btnB)"></button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import json from '../assets/data.json'
+
+export default {
+  data(){
+    return {
+      msgTable : [],
+      storagedMsg : null,
+      memoTable : [], // table to set the local storage 
+      test : true,
+
+    }
+  },
+};
+</script>
--- app/router.js
+++ app/router.js
@@ -2,8 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from './components/Home.vue';
-import Page from './components/Page.vue';
-
 Vue.use(Router);
 
 const router = new Router({
