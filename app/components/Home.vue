870033193a47175e11dbebc75d1993f0d1879138
Elhamdani <spartan9452@gmail.com>
2019-05-20 09:37:33 +0200

    d

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -2,7 +2,8 @@
   <div class="big-header">
     <h1>Bienvenue à HETIC</h1>
     <p>Ca y est vous êtes admis dans l’école prestigieuse qu’est HETIC, Brontis vous accueille pour votre premier jour.
-Survivez à cette année sans vous faire virer, et validez votre année. Faites les bons choix pour espérer finir avec une note supérieure à 10/20</p>
+       Survivez à cette année sans vous faire virer, et validez votre année. Faites les bons choix pour espérer finir avec une note supérieure à 10/20
+    </p>
     <br />
     <router-link class="button" to="/page">Go to Page</router-link>
   </div>
