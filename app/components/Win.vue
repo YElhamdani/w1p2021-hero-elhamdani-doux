0e14ec428c5d88392e4e16f6713ce0803916b54a
Elhamdani <spartan9452@gmail.com>
2019-05-23 18:37:36 +0200

    site finis à 75%

--- app/components/Win.vue
+++ app/components/Win.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="big-header">
-    <h1>Win</h1>
+    <h1>Félicitation !<br> <br> vous avez terminé votre année avec succès, vous l’avez validée</h1>
     <br />
     <router-link class="button" to="/">Go to Home</router-link>
   </div>
